87f326604ef8c8350b001c8c4e9fa26a1c90b0a1
HyugaNeji <13666134940@163.com>
2021-05-19 09:00:37 +0800

    测试IDEA的Git

--- Redis/PublishSubscribe/RedisPublish/src/main/java/com/publish/RedisPublish.java
+++ Redis/PublishSubscribe/RedisPublish/src/main/java/com/publish/RedisPublish.java
@@ -12,7 +12,7 @@ public class RedisPublish {
         Jedis jedis = new Jedis("10.74.40.3", 6379);
         jedis.publish("CCTV-1", "我们将于下月中旬上映《宝宝巴士》");
         System.out.println("消息发布完毕..");
-        
+
     }
 
 }
